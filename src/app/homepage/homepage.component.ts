8d59c716b7eea11322daf39ec4de3521f41354ce
Chazona Baum <chznbaum@gmail.com>
2017-10-08 13:59:50 -0400

    implemented homepage component

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: [ './homepage.component.scss' ]
+})
+
+export class HomepageComponent {}
